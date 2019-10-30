7c28473325e7820d59d0e8bbc40889fbc07e17a2
Patryk Ruszaj <patryk.ruszaj@yahoo.com>
2019-10-30 21:19:45 +0100

    feature: Authorization

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -62,7 +62,7 @@ return [
     */
 
     'cache' => [
-        'enable' => env('LIGHTHOUSE_CACHE_ENABLE', true),
+        'enable' => env('LIGHTHOUSE_CACHE_ENABLE', false),
         'key' => env('LIGHTHOUSE_CACHE_KEY', 'lighthouse-schema'),
         'ttl' => env('LIGHTHOUSE_CACHE_TTL', null),
     ],
